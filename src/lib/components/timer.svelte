b0032ba7eb2b04ee2a3e5fff5a7c245d59f4a646
Emanuel Bodin <bodinemanuel@gmail.com>
2023-08-25 16:57:14 +0200

    hej

---
+++ src/lib/components/timer.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+    import { timer, time, isRunning, isComplete } from "$lib/stores/timer";
+
+</script>
+
+<div class="text-white text-2xl">{$time}</div>
+  <div class="buttons">
+    {#if !$isComplete}
+      {#if $isRunning}
+        <button class='btn btn-outline btn-warning' on:click={() => timer.pause()}>pause</button>
+      {:else}
+        <button class='btn btn-outline btn-success' on:click={() => timer.start()}>start</button>
+      {/if}
+    {/if}
+  </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang='ts'>
+	import Timer from '$lib/components/timer.svelte';
+	import tupp from '$lib/images/tupp.svg'
 </script>
 
 <svelte:head>
@@ -9,51 +8,10 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class='flex flex-col gap-2 justify-center items-center'>
+	<picture>
+		<source srcset={tupp} type="image/webp" />
+		<img src={tupp} alt="Welcome" />
+	</picture>
+	<Timer/>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
